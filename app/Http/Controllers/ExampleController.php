8703d9e7749fd9d2f3f32c5d7122f16312d4cba9
Jogi Silalahi <jsilalahi@dyned.com>
2018-11-08 16:19:29 +0700

    asd

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -4,15 +4,6 @@ namespace App\Http\Controllers;
 
 class ExampleController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
 
 
     /**
